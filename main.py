e55741a51d181976342af901a1704405232cc51d
Adrián Ayuso-Muñoz <180382013+amadrian00@users.noreply.github.com>
2025-05-09 16:08:01 +0900

    Update main.py comparison.

--- main.py
+++ main.py
@@ -36,13 +36,13 @@ def prepare_dataset(dataset_name, label_col=0, check_nan=False):
 def create_data_loader(dataset, indices, batch_size=64, shuffle=False):
     return DataLoader(dataset[indices], batch_size=batch_size, shuffle=shuffle)
 
-def train_test_kfold(dataset, y, train_id, test_loader, train_loader_ft=None):
-    skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
+def train_test_kfold(dataset, y, train_id, test_loader_ext, train_loader_ft=None):
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
     all_metrics = []
     for fold, (train_idx, _) in enumerate(skf.split(torch.zeros_like(y), y)):
         print(f'\n--- Fold {fold + 1} ---')
 
-        train_loader = DataLoader(dataset[train_id[train_idx]], batch_size=64, shuffle=True)
+        train_loader = create_data_loader(dataset, train_id[train_idx], shuffle=True)
 
         model = GCN(9, 1).to(device)
         optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
@@ -58,43 +58,54 @@ def train_test_kfold(dataset, y, train_id, test_loader, train_loader_ft=None):
             model.lin.reset_parameters()
             optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0.0005)
 
-            train(model, train_loader_ft, optimizer, criterion, device, num_epochs=50)
+            train(model, train_loader_ft, optimizer, criterion, device, num_epochs=20)
 
-        metrics = evaluate(model, test_loader, device)
+        metrics = evaluate(model, test_loader_ext, device)
         all_metrics.append(metrics)
         print(metrics)
     return all_metrics
 
-
 if __name__ == '__main__':
     add_safe_globals([DataEdgeAttr, DataTensorAttr, GlobalStorage])
-    set_seed(42)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    ####################################################################################################
-    ###     This test loader MUST not be changed and SHOULD be used for reporting final results.     ###
-    ####################################################################################################
-    dataset_bace, split_bace, idx_bace, y_bace = prepare_dataset("ogbg-molbace")
-    test_loader_bace = create_data_loader(dataset_bace, split_bace["test"])
-    ####################################################################################################
-    ####################################################################################################
+    seeds = [42, 84, 126, 168, 210]
 
-    train_loader_bace = create_data_loader(dataset_bace, idx_bace, shuffle=True)
+    all_metrics_bace_all_runs = []
+    all_metrics_bbbp_all_runs = []
 
-    all_metrics_bace = train_test_kfold(dataset_bace, y_bace, idx_bace, test_loader_bace)
+    for seed in seeds:
+        print(f"\nRunning with seed: {seed}")
+        set_seed(seed)
 
-    dataset_bbbp, split_bbbp, idx_bbbp, y_bbbp = prepare_dataset("ogbg-molbbbp", check_nan=True)
+        ####################################################################################################
+        ###     This test loader MUST not be changed and SHOULD be used for reporting final results.     ###
+        ####################################################################################################
+        dataset_bace, split_bace, idx_bace, y_bace = prepare_dataset("ogbg-molbace")
+        test_loader_bace = create_data_loader(dataset_bace, split_bace["test"])
+        ####################################################################################################
+        ####################################################################################################
 
-    all_metrics_bbbp = train_test_kfold(dataset_bbbp, y_bbbp, idx_bbbp, train_loader_bace, test_loader_bace)
+        train_loader_bace = create_data_loader(dataset_bace, idx_bace, shuffle=True)
+        all_metrics_bace = train_test_kfold(dataset_bace, y_bace, idx_bace, test_loader_bace)
+        all_metrics_bace_all_runs.append(all_metrics_bace)
+
+        dataset_bbbp, split_bbbp, idx_bbbp, y_bbbp = prepare_dataset("ogbg-molbbbp", check_nan=True)
+        all_metrics_bbbp = train_test_kfold(dataset_bbbp, y_bbbp, idx_bbbp, test_loader_bace, train_loader_ft=train_loader_bace)
+        all_metrics_bbbp_all_runs.append(all_metrics_bbbp)
+
+    flatten = lambda l: [item for sublist in l for item in sublist]
+    flat_bace = flatten(all_metrics_bace_all_runs)
+    flat_bbbp = flatten(all_metrics_bbbp_all_runs)
 
     print("\n--- 5 Folds Average + Paired t-test ---")
     print(
         f"{'Metric':<15}{'Mean BACE':>12}{'Std BACE':>12}{'Mean BBBP':>15}{'Std BBBP':>12}{'% Inc.':>15}{'p-value':>12}")
     print("-" * 99)
 
-    for key in all_metrics_bace[0].keys():
-        values_bace = [m[key] for m in all_metrics_bace]
-        values_bbbp = [m[key] for m in all_metrics_bbbp]
+    for key in flat_bace[0].keys():
+        values_bace = [m[key] for m in flat_bace]
+        values_bbbp = [m[key] for m in flat_bbbp]
 
         mean_bace = sum(values_bace) / len(values_bace)
         std_bace = tstd(values_bace)
